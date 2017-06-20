3f8695e02adff5675f18f2802767f8bd139c2e6d
Justin Hart <justinhart90@gmail.com>
2017-06-20 11:00:23 -0600

    🤙 added experience section, images, and styling

--- app/public/js/eventListeners.js
+++ app/public/js/eventListeners.js
@@ -2,10 +2,18 @@ $(document).ready( () => {
   $("#nav-text").mouseover( () => {
     let arrow = $("<img id='down-arrow' src='../images/down.png'>");
     $("#nav-text").html(arrow);
+    $("#nav-text").mouseleave( () => {
+      // let message = $("<span></span>");
+      $("#nav-text").html();
+      $("#nav-text").text("the stuff to know is down below");
+    })
   })
-  $("#nav-text").mouseleave( () => {
-    // let message = $("<span></span>");
-    $("#nav-text").html();
-    $("#nav-text").text("the stuff to know is down below");
+  $("#nav-link").click( (e) => {
+    e.preventDefault();
+    scrollTo("#nav-link");
   })
 })
+
+function scrollTo(hash) {
+    location.hash = "#" + hash;
+}
