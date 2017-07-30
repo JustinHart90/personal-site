84b6e456a8cd9888ba471d83abd306ea8306ac92
Justin Hart <justinhart90@gmail.com>
2017-07-30 16:56:43 -0600

    adjusted responsiveness

--- app/public/js/eventListeners.js
+++ app/public/js/eventListeners.js
@@ -1,19 +1,16 @@
 $(document).ready( () => {
-  $("#nav-text").mouseover( () => {
+  $("#nav-text").mouseover(() => {
     let arrow = $("<img id='down-arrow' src='../images/down.png'>");
     $("#nav-text").html(arrow);
-    $("#nav-text").mouseleave( () => {
-      // let message = $("<span></span>");
+    $("#nav-text").mouseleave(() => {
       $("#nav-text").html();
       $("#nav-text").text("the stuff to know is down below");
     })
   })
-  $("#nav-link").click( (e) => {
+  $("#nav-link").click(e => {
     e.preventDefault();
     scrollTo("#nav-link");
   })
-  // $('.ui.labeled.icon.sidebar')
-  // .sidebar('toggle');
 })
 
 function scrollTo(hash) {
