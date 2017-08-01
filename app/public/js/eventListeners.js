725236719556a1592b4be399c6ab4a32bec4af7f
Justin Hart <justinhart90@gmail.com>
2017-08-01 17:49:00 -0600

    added flip annimation

--- app/public/js/eventListeners.js
+++ app/public/js/eventListeners.js
@@ -11,6 +11,25 @@ $(document).ready( () => {
     e.preventDefault();
     scrollTo("#nav-link");
   })
+
+  let mockHeight = $("#coin-mock").height();
+  $(".col-lg-7").height(mockHeight + 25);
+  let newHeight = $(".col-lg-7").height();
+
+  $(".flip-to-back").click(e => {
+    e.preventDefault();
+    $("#card").addClass("flipped");
+    $("#card2").addClass("flipped");
+    $("#card3").addClass("flipped");
+    $("#card4").addClass("flipped");
+  })
+  $(".flip-to-front").click(e => {
+    e.preventDefault();
+    $("#card").removeClass("flipped");
+    $("#card2").removeClass("flipped");
+    $("#card3").removeClass("flipped");
+    $("#card4").removeClass("flipped");
+  })
 })
 
 function scrollTo(hash) {
